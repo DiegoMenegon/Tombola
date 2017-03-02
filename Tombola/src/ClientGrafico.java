49ae1af8a8a09e219aae3a26c7e4d49abb05af75
gallinagreta <gallinagreta@Lab06_03.itseinaudi.local>
2017-03-02 09:45:39 +0100

    diego sf

--- Tombola/src/ClientGrafico.java
+++ Tombola/src/ClientGrafico.java
@@ -1,9 +1,23 @@
+import java.awt.Button;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.custom.TableCursor;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class ClientGrafico {
 
 	protected Shell shell;
+	private Table table;
 
 	/**
 	 * Launch the application.
@@ -38,9 +52,77 @@ public class ClientGrafico {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
+		shell.setSize(671, 380);
 		shell.setText("SWT Application");
+		
+		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setBounds(10, 87, 454, 84);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn.setWidth(50);
+		
+		TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_1.setWidth(50);
+		
+		TableColumn tblclmnNewColumn_2 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_2.setWidth(50);
+		
+		TableColumn tblclmnNewColumn_3 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_3.setWidth(50);
+		
+		TableColumn tblclmnNewColumn_4 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_4.setWidth(50);
+		
+		TableColumn tblclmnNewColumn_5 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_5.setWidth(50);
+		
+		TableColumn tblclmnNewColumn_6 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_6.setWidth(50);
+		
+		TableColumn tblclmnNewColumn_7 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_7.setWidth(50);
+		
+		TableColumn tblclmnNewColumn_8 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_8.setWidth(50);
+		
+		try {
+			Socket s = new Socket("172.16.6.4", 9080);
+
+					} catch (UnknownHostException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		
+		
+		org.eclipse.swt.widgets.Button btnRecuperaNumeri = new org.eclipse.swt.widgets.Button(shell, SWT.NONE);
+		btnRecuperaNumeri.setBounds(10, 201, 111, 25);
+		btnRecuperaNumeri.setText("Recupera numeri");
+  		btnRecuperaNumeri.addSelectionListener(new SelectionAdapter() {
+  			@Override
+  			public void widgetSelected(SelectionEvent e) {
+  				// Apre il socket
+ 				try {
+ 					Socket s = new Socket("localhost", 9999);
+ 					for (int i = 0; i < 15; i++) {
+ 						System.out.print(s.getInputStream().read() + " ");
+ 					}
+ 				} catch (IOException e1) {
+ 					// TODO Auto-generated catch block
+ 					e1.printStackTrace();
+ 				}
+ 
+  				// Riceve i 15 numeri
+ 				// Apre il thread di comunicazione che riceverà i comandi successivi
+ 				// Apre il thread di comunicazione che riceverà i comandi
+ 				// successivi
+  			}
+  		});
+
 
 	}
-
 }
